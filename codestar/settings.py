f7cc5b78663472b0518ab27f02b0d9fdd6d2eca4
pooja-par <pooja.par92@gmail.com>
2024-09-18 01:11:47 +0200

    admin page with post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hz8*-jc=tpm1l0d2kv^3*4eee4!2$-f%=1a#v8)h!z^oy=5saf'
+#SECRET_KEY = 'django-insecure-hz8*-jc=tpm1l0d2kv^3*4eee4!2$-f%=1a#v8)h!z^oy=5saf'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://127.0.0.1:8000/", '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -89,6 +91,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
